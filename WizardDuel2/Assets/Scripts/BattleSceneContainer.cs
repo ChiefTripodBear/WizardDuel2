1e54fbcbae226ec2dc76d22a9f4f1577e4f5e60d
ChiefTripodBear <avalacos@gmail.com>
2018-09-04 17:13:32 +1000

    Continued UI interaction
    Turning on and off certain canvases based on round phase.
    
    there's a bug in the sorting layer with the health/manabars.

---
+++ WizardDuel2/Assets/Scripts/BattleSceneContainer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneContainer : MonoBehaviour
+{
+    GameManager gameManager;
+    GameObject screenDisplayChild;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+        screenDisplayChild = transform.GetChild(0).gameObject;
+        screenDisplayChild.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (!gameManager.spellcastingEnabled)
+        {
+            screenDisplayChild.gameObject.SetActive(true);
+        }
+        else
+        {
+            screenDisplayChild.gameObject.SetActive(false);
+        }
+    }
+}
